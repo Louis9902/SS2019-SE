8d52b11b175014e8a67c023eec685191cc1558d6
JensWoe <woehler.jens@gmx.de>
2019-05-09 16:41:08 +0200

    Added VS project and first GUI

---
+++ Implementierung/GUI/GUI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static string ConfigFilePath = "";
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+            MessageBox.Show("Load config here");
+        }
+
+        private void Button_OK_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Do stuff here");
+            this.Close();
+        }
+
+        private void Button_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
